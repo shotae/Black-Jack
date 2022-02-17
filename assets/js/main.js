ae94e655666732f05b0698d2eab07f9e80ce7546
air67sh <air67sh@penguin.lxd>
2022-02-18 08:10:10 +0900

    split fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,33 +29,21 @@ function cardShuffle() {
 let dealCounter = 0;
 let dealerPoints = 0;
 const dealersHand = [];
+
 function dealDealer() {
   for (let i = 0; i < 2; i++) {
-    dealersHand.push(cards[dealCounter++]);
-    const cardNum = dealersHand[i].num;
-    const cardMark = dealersHand[i].mark;
-    if (i === 0) {
-      $('#dealer-cards').append(`<div class='card-animation reverse ${cardMark}${cardNum}'></div>`).fadeIn('slow');
-    } else {
-      $('#dealer-cards').append(`<div class='card-animation ${cardMark}${cardNum}'></div>`).fadeIn('slow');
-    }
+    cardDeal(dealersHand);
   };
-  dealerPoints = pointCheck(dealersHand);
-  $('#dealer-points').append(`<div>Points: ???</div>`);
 };
 
 // プレイヤーが2枚ドロー
 let playerPoints = 0;
 const playersHand = [];
+
 function dealPlayer() {
   for (let i = 0; i < 2; i++) {
-    playersHand.push(cards[dealCounter++]);
-    const cardNum = playersHand[i].num;
-    const cardMark = playersHand[i].mark;
-    $('#player-cards').append(`<div class='card-animation ${cardMark}${cardNum}'></div>`);
+    cardDeal(playersHand);
   };
-  playerPoints = pointCheck(playersHand);
-  $('#player-points').append(`<div>Points: ${playerPoints}</div>`);
 
   $('button').removeClass('cant-use');
 };
@@ -67,39 +55,125 @@ function pointCheck(array) {
   for (let i = 0; i < array.length; i++) {
     let point = 0;
     point = array[i].num;
-    if (point >= 11) {
-      point = 10;
-    };
+    if (point >= 11) point = 10;
     totalPoints += point;
   }
   for (let i = 0; i < array.length; i++) {
-    if (array[i].num === 1 && totalPoints <= 11){
-      totalPoints += 10;
-    }
+    if (array[i].num === 1 && totalPoints <= 11) totalPoints += 10;
   }
   return totalPoints;
 }
 
-//  プレイヤーがヒット（1枚引く）
+// split
+let splitFlag = 0;
+const splitPlayersHand1 = [];
+const splitPlayersHand2 = [];
+$('#split-button').on('click', function() {
+  $('#split-button').addClass('cant-use');
+  if ((playersHand[0].num === playersHand[1].num) ||
+      (playersHand[0].num >= 10 && playersHand[1].num >= 10)) {
+    splitPlayersHand1[0] = playersHand[0];
+    splitPlayersHand2[0] = playersHand[1];
+    playersHand.length = 0;
+    splitFlag = 1;
 
-$('#hit-button').on('click', function() {
-  playersHand.push(cards[dealCounter]);
+    $('.first-player-deal').remove();
+    let cardNum = splitPlayersHand1[0].num;
+    let cardMark = splitPlayersHand1[0].mark;
+    $('#player-cards1').append(`<div class='card-animation ${cardMark}${cardNum}'></div>`);
+    tmpPoints = pointCheck(splitPlayersHand1);
+    $('#player-points1').text(`Points: ${tmpPoints}`)
 
-  const playersLastestcard = dealCounter++ - dealersHand.length
-  const cardNum = playersHand[playersLastestcard].num;
-  const cardMark = playersHand[playersLastestcard].mark;
-  $('#player-cards').append(`<div class='card-animation ${cardMark}${cardNum}'></div>`);
+    cardNum = splitPlayersHand2[0].num;
+    cardMark = splitPlayersHand2[0].mark;
+    $('#player-cards2').append(`<div class='card-animation ${cardMark}${cardNum}'></div>`);
+    tmpPoints = pointCheck(splitPlayersHand2);
+    $('#player-points2').text(`Points: ${tmpPoints}`)
 
-  playerPoints = pointCheck(playersHand);
-  $('#player-points > div').text(`Points: ${playerPoints}`);
-
-  if (playerPoints > 21) {
-    burstHand('player');
+    $('.cards-wrap1, .cards-wrap2').removeClass('split-hidden');
+    $('.cards-wrap').addClass('split-hidden');
   };
 });
+  
+//  プレイヤーがヒット（1枚引く）
+let splitPlayersPoints1 = 0;
+let splitPlayersPoints2 = 0;
+$('#hit-button').on('click', function() {
+  if (splitFlag === 0) {
+    cardDeal(playersHand);
+    playerPoints = tmpPoints;
+  } else if (splitFlag === 1) {
+    cardDeal(splitPlayersHand1);
+    splitPlayersPoints1 = tmpPoints;
+  } else if (splitFlag === 2) {
+    cardDeal(splitPlayersHand2);
+    splitPlayersPoints2 = tmpPoints;
+  }
+});
+
+// カードを配る
+let lastCard = 0;
+let tmpPoints = 0;
+let $cardsHolder = '';
+let $pointsHolder = '';
+function cardDeal(arry) {
+  arry.push(cards[dealCounter]);
+
+
+  if (arry === playersHand) {
+    lastCard = dealCounter++ - dealersHand.length;
+    $cardsHolder = $('#player-cards');
+    $pointsHolder = $('#player-points');
+
+  } else if (arry === splitPlayersHand1) {
+    lastCard = dealCounter++ - dealersHand.length - splitPlayersHand2.length;
+    $cardsHolder = $('#player-cards1');
+    $pointsHolder = $('#player-points1');
+
+  } else if (arry === splitPlayersHand2) {
+    lastCard = dealCounter++ - dealersHand.length - splitPlayersHand1.length;
+    $cardsHolder = $('#player-cards2');
+    $pointsHolder = $('#player-points2');
+
+  } else if (arry === dealersHand) {
+    lastCard = dealCounter++ - playersHand.length - splitPlayersHand1.length - splitPlayersHand2.length;
+    $cardsHolder = $('#dealer-cards');
+    $pointsHolder = $('#dealer-points');
+  }
+
+  const cardNum = arry[lastCard].num;
+  const cardMark = arry[lastCard].mark;
+
+  if (dealCounter === 1) {
+    $cardsHolder.append(`<div class='card-animation reverse ${cardMark}${cardNum}'></div>`);
+  } else if (dealCounter === 3 || dealCounter === 4) {
+    $cardsHolder.append(`<div class='card-animation first-player-deal ${cardMark}${cardNum}'></div>`);
+  } else {
+    $cardsHolder.append(`<div class='card-animation ${cardMark}${cardNum}'></div>`);
+  }
+
+  tmpPoints = pointCheck(arry);
+  displayPoint(arry);
+}
+
+// 得点を表示
+function displayPoint(arry) {
+  tmpPoints = pointCheck(arry);
+  if (arry === dealersHand) {
+    $pointsHolder.text(`Points: ??`);
+  } else {
+    $pointsHolder.text(`Points: ${tmpPoints}`);
+  }
+
+  if (tmpPoints > 21) burstHand(arry);
+}
 
 // プレイヤーがスタンド
 $('#stand-button').on('click', function() {
+  if (splitFlag === 1) {
+    splitFlag = 2;
+    return;
+  };
   $('#hit-button').addClass('cant-use');
   dealerHit();
 });
@@ -107,63 +181,99 @@ $('#stand-button').on('click', function() {
 // ＆ ディーラーが可能な限りヒット
 function dealerHit() {
   dealerPoints = pointCheck(dealersHand);
-  while(dealerPoints <= 17) {
-    dealersHand.push(cards[dealCounter]);
-
-    const dealersLastestcard = dealCounter++ - playersHand.length;
-    const cardNum = dealersHand[dealersLastestcard].num;
-    const cardMark = dealersHand[dealersLastestcard].mark;
-    $('#dealer-cards').append(`<div class='card-animation ${cardMark}${cardNum}'></div>`);
-
-    dealerPoints = pointCheck(dealersHand);
-    $('#dealer-points').text(`Points: ${dealerPoints}`);
+  while (dealerPoints <= 17) {
+    cardDeal(dealersHand);
+    dealerPoints = tmpPoints;
   };
-
-  if (dealerPoints > 21) {
-    burstHand('dealer');
-  } else {
-    decideWinner();
-  }
+  if (dealerBurstFlag === 0) decideWinner();
 };
 
 // 勝敗を決める
 function decideWinner() {
   dealerCardOpen();
-  if (playerPoints > dealerPoints) {
-    $('#result').append('<div>プレイヤーの勝利</div>');
-  } else {
-    $('#result').append('<div>ディーラーの勝利</div>');
-  };
+  playerPoints = pointCheck(playersHand);
+  splitPlayersPoints1 = pointCheck(splitPlayersHand1);
+  splitPlayersPoints2 = pointCheck(splitPlayersHand2);
+  dealerPoints = pointCheck(dealersHand);
+
+  if (splitFlag === 0) {
+    if (playerPoints > dealerPoints || dealerBurstFlag === 1) {
+      $('#result').append('<div>勝利</div>');
+    } else {
+      $('#result').append('<div>敗北</div>');;
+    }
+  } else if (splitFlag !== 0) {
+    if (splitPlayersPoints1 > dealerPoints) {
+      $('#result').append('<div>手札1:勝利</div>');
+    } else {
+      $('#result').append('<div>手札1:敗北</div>');
+    };
+    if (splitPlayersPoints2 > dealerPoints && splitPlayersPoints2 <= 21) {
+      $('#result').append('<div>手札2:勝利</div>');
+    } else {
+      $('#result').append('<div>手札2:敗北</div>');
+    };
+  }
   $('#stand-button').addClass('cant-use');
 }
 
 // バーストの処理
+let splitBurstFlag  = 0;
+let dealerBurstFlag  = 0;
 function burstHand (burster) {
-  dealerCardOpen();
-  if (burster === 'player') {
-    $('#result').append('<div>プレイヤーの手札がバースト<br>ディーラーの勝利</div>');
-  } else if (burster === 'dealer') {
-    $('#result').append('<div>ディーラーの手札がバースト<br>プレイヤーの勝利</div>');
+  if (burster === playersHand) {
+    $('#result').append('<div>プレイヤーの手札がバースト<br>敗北</div>');
+    $('#stand-button').addClass('cant-use');
+    $('#hit-button').addClass('cant-use');
+
+  } else if (burster === splitPlayersHand1){
+    $('#result').append('<div>プレイヤーの手札がバースト1</div>');
+    splitFlag = 2;
+    splitBurstFlag = 1;
+    return;
+
+  } else if (burster === splitPlayersHand2){
+    $('#result').append('<div>プレイヤーの手札がバースト2</div>');
+    if (splitBurstFlag === 0) {
+      dealerHit();
+    } else {
+      $('#result').append('<div>手札2つバーストにつき敗北</div>');;
+    };
+
+  } else if (burster === dealersHand) {
+    $('#result').append('<div>ディーラーの手札がバースト<br>勝利</div>');
+    dealerBurstFlag = 1;
   }
+
   $('#stand-button').addClass('cant-use');
-  $('#hit-button').addClass('cant-use')
+  $('#hit-button').addClass('cant-use');
+  dealerCardOpen();
 }
 
+// ディーラーのカードオープン
 function dealerCardOpen() {
   $('.reverse').removeClass('reverse');
+  dealerPoints = pointCheck(dealersHand);
   $('#dealer-points').text(`Points: ${dealerPoints}`);
 }
 
+// start
 $('#start-game').on('click', function() {
   $(this).addClass('new-game');
   $(this).text('New Game')
   $('button').addClass('cant-use');
   $('.hidden').removeClass('hidden');
+  $('.cards-wrap1, .cards-wrap').addClass('split-hidden');
+  $('.cards-wrap').removeClass('split-hidden');
   dealCounter = 0;
   cards.length = 0;
   dealersHand.length = 0;
   playersHand.length = 0;
-  $('.restart').empty();
+  splitPlayersHand1.length = 0;
+  splitPlayersHand2.length = 0;
+  splitFlag = 0;
+  dealerBurstFlag = 0;
+  $('.html-reset').empty();
   prepareCards();
   cardShuffle();
   dealDealer();
